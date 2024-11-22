5bf3b8aa4c30c2d11a5e896085c8a10cca5a9189
minhazapon <minhajulabedinapon@gmail.com>
2024-11-21 21:49:52 -0800

    added file

--- src/Home file/Provide.tsx
+++ src/Home file/Provide.tsx
@@ -10,15 +10,26 @@ const Provide = () => {
             <div>
 
 
+               
+               <div>
                <div className="  ">
                <img className=" h-[500px] w-[600px]  " src="https://media.licdn.com/dms/image/v2/C561BAQF7LivwcMv9DQ/company-background_10000/company-background_10000/0/1583891737912?e=2147483647&v=beta&t=kTwoVvfOF7R-PVBmfiCw5unJO4AgV7x1q80hnszyQio" alt="" />
                </div>
-               <div className=" flex-col  mt-5 border-[2px] border-yellow-500 w-[600px] ">
+               <div className=" flex-col  mt-5 border-[2px] border-yellow-500 w-[600px] p-3 ">
                <div className=" flex items-center justify-center gap-10">
                <p><FaPeopleRoof className=" h-[60px] w-[60px] "></FaPeopleRoof></p>
                <p className=" text-5xl ">30+</p>
                </div>
                <p className=" font-serif text-center text-xl mt-4 ">Years of Experience</p>
+               </div>
+               </div>
+
+
+               <div>
+
+
+
+                
                </div>
 
 
