f2eb5b51979ea71117cba9ae598077cb667273bd
minhazapon <minhajulabedinapon@gmail.com>
2024-11-24 21:16:12 -0800

    added file

---
+++ src/Home file/PricePlan.tsx
@@ -0,0 +1,149 @@
+
+
+
+const PricePlan = () => {
+
+
+    return (
+        <div className=" mt-14 mb-14 ml-24 mr-24">
+
+            <div>
+                 
+                 <div>
+                   <p className=" text-center text-yellow-500 font-semibold ">OUR PRICING PLAN</p>
+                   <p className=" text-center font-bold text-5xl mt-2  universe  ">Best pricing plans that<br></br>
+                   fits for you</p>
+                 </div>
+
+
+               
+
+                 <div className=" flex justify-center items-center gap-10 mt-10  ">
+
+
+                 <div className=" border-[1px] w-fit p-14   "
+                 style={{
+                    backgroundImage: "url(https://png.pngtree.com/thumb_back/fh260/background/20210408/pngtree-white-abstract-vector-web-background-design-image_597636.jpg)",
+                  }}
+                 > 
+                 <div className="  "
+                 > 
+                 <p className=" text-3xl font-bold soft ">Basic Plan</p>
+                 <p className=" mt-5 text-gray-500 ">Designed for businesses with basic<br></br> IT requirements</p>
+                 <p className=" mt-5 bg-gray-300 h-[2px] w-[220px] "></p> 
+                 <div className=" mt-10 ">
+                 <p className="  "><span className=" font-bold text-5xl soft ">$59</span> <span className=" font-semibold text-gray-400 "  >/month</span></p>
+                 </div>
+                 <p className=" text-xl text-yellow-500 mt-8 " >All Basic services include:</p>
+                 <div className=" mt-10">
+                 <div className=" flex items-center gap-3 ">
+                 <img className=" h-[30px] " src="https://cdn-icons-png.flaticon.com/128/2722/2722998.png" alt="" />
+                 <p className=" text-black the ">24/7 system monitoring</p>
+                 </div>
+                 <div className=" mt-3 flex items-center gap-3 ">
+                 <img className=" h-[30px] " src="https://cdn-icons-png.flaticon.com/128/2722/2722998.png" alt="" />
+                 <p className=" text-black the ">Security management</p>
+                 </div>
+                 <div className=" mt-3 flex items-center gap-3 ">
+                 <img className=" h-[30px] " src="https://cdn-icons-png.flaticon.com/128/2722/2722998.png" alt="" />
+                 <p className=" text-gray-400 the  line-through ">Patch management</p>
+                 </div>
+                 <div className=" mt-3 flex items-center gap-3 ">
+                 <img className=" h-[30px] " src="https://cdn-icons-png.flaticon.com/128/2722/2722998.png" alt="" />
+                 <p className=" text-gray-400 the line-through ">Remote support</p>
+                 </div>
+                 </div>
+                 </div>
+                 </div>
+
+
+                 <div className=" border-[1px] w-fit p-14   "
+                 style={{
+                    backgroundImage: "url(https://png.pngtree.com/thumb_back/fh260/background/20210408/pngtree-white-abstract-vector-web-background-design-image_597636.jpg)",
+                  }}
+                 > 
+                 <div className="  "
+                 > 
+                 <p className=" text-3xl font-bold soft ">BUSINESS Plan</p>
+                 <p className=" mt-5 text-gray-500 ">Designed for businesses with basic<br></br> IT requirements</p>
+                 <p className=" mt-5 bg-gray-300 h-[2px] w-[220px] "></p> 
+                 <div className=" mt-10 ">
+                 <p className="  "><span className=" font-bold text-5xl soft ">$99</span> <span className=" font-semibold text-gray-400 "  >/month</span></p>
+                 </div>
+                 <p className=" text-xl text-yellow-500 mt-8 " >All Basic services include:</p>
+                 <div className=" mt-10">
+                 <div className=" flex items-center gap-3 ">
+                 <img className=" h-[30px] " src="https://cdn-icons-png.flaticon.com/128/2722/2722998.png" alt="" />
+                 <p className=" text-black the ">24/7 system monitoring</p>
+                 </div>
+                 <div className=" mt-3 flex items-center gap-3 ">
+                 <img className=" h-[30px] " src="https://cdn-icons-png.flaticon.com/128/2722/2722998.png" alt="" />
+                 <p className=" text-black the ">Security management</p>
+                 </div>
+                 <div className=" mt-3 flex items-center gap-3 ">
+                 <img className=" h-[30px] " src="https://cdn-icons-png.flaticon.com/128/2722/2722998.png" alt="" />
+                 <p className=" text-gray-400 the  line-through ">Patch management</p>
+                 </div>
+                 <div className=" mt-3 flex items-center gap-3 ">
+                 <img className=" h-[30px] " src="https://cdn-icons-png.flaticon.com/128/2722/2722998.png" alt="" />
+                 <p className=" text-gray-400 the line-through ">Remote support</p>
+                 </div>
+                 </div>
+                 </div>
+                 </div>
+
+
+                 <div className=" border-[1px] w-fit p-14   "
+                 style={{
+                    backgroundImage: "url(https://png.pngtree.com/thumb_back/fh260/background/20210408/pngtree-white-abstract-vector-web-background-design-image_597636.jpg)",
+                  }}
+                 > 
+                 <div className="  "
+                 > 
+                 <p className=" text-3xl font-bold soft ">Basic Plan</p>
+                 <p className=" mt-5 text-gray-500 ">Designed for businesses with basic<br></br> IT requirements</p>
+                 <p className=" mt-5 bg-gray-300 h-[2px] w-[220px] "></p> 
+                 <div className=" mt-10 ">
+                 <p className="  "><span className=" font-bold text-5xl soft ">$59</span> <span className=" font-semibold text-gray-400 "  >/month</span></p>
+                 </div>
+                 <p className=" text-xl text-yellow-500 mt-8 " >All Basic services include:</p>
+                 <div className=" mt-10">
+                 <div className=" flex items-center gap-3 ">
+                 <img className=" h-[30px] " src="https://cdn-icons-png.flaticon.com/128/2722/2722998.png" alt="" />
+                 <p className=" text-black the ">24/7 system monitoring</p>
+                 </div>
+                 <div className=" mt-3 flex items-center gap-3 ">
+                 <img className=" h-[30px] " src="https://cdn-icons-png.flaticon.com/128/2722/2722998.png" alt="" />
+                 <p className=" text-black the ">Security management</p>
+                 </div>
+                 <div className=" mt-3 flex items-center gap-3 ">
+                 <img className=" h-[30px] " src="https://cdn-icons-png.flaticon.com/128/2722/2722998.png" alt="" />
+                 <p className=" text-gray-400 the  line-through ">Patch management</p>
+                 </div>
+                 <div className=" mt-3 flex items-center gap-3 ">
+                 <img className=" h-[30px] " src="https://cdn-icons-png.flaticon.com/128/2722/2722998.png" alt="" />
+                 <p className=" text-gray-400 the line-through ">Remote support</p>
+                 </div>
+                 </div>
+                 </div>
+                 </div>
+
+
+
+
+
+                 </div>
+
+
+
+
+
+
+
+            </div>
+            
+        </div>
+    );
+};
+
+export default PricePlan;
--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,7 @@
 import HomeBanner from "./Home file/HomeBanner";
 import InfoCard from "./Home file/InfoCard";
 import Memberz from "./Home file/Memberz";
+import PricePlan from "./Home file/PricePlan";
 import Provide from "./Home file/Provide";
 import Service from "./Home file/Service";
 import Technology from "./Home file/Technology";
@@ -17,6 +18,7 @@ const Home = () => {
               <Technology></Technology>
               <Memberz></Memberz>
               <Service></Service>
+              <PricePlan></PricePlan>
             
         </div>
     );
